10cc592861b44eebd99e95d96813033561413a95
leoata <leonardo@atalla.org>
2022-05-05 13:45:46 -0400

    fix errors

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,10 +4,11 @@ import {AppProps} from 'next/app';
 import {ThemeProvider} from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import {CacheProvider, EmotionCache} from '@emotion/react';
-import theme from '../src/theme';
-import createEmotionCache from '../src/createEmotionCache';
-import "../src/styles/globals.css";
+import theme from '../theme';
+import createEmotionCache from '../createEmotionCache';
+import "../styles/globals.css";
 import {SessionProvider} from "next-auth/react";
+import {GlobalStateProvider} from '../store';
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
 
@@ -15,21 +16,28 @@ interface MyAppProps extends AppProps {
     emotionCache?: EmotionCache;
 }
 
-export default function MyApp(props: MyAppProps) {
-    const {Component, emotionCache = clientSideEmotionCache, pageProps: {session, ...pageProps} } = props;
+export default function MyApp({
+                                  Component,
+                                  emotionCache = clientSideEmotionCache,
+                                  pageProps: {session, ...pageProps},
+                                  router
+                              }: MyAppProps) {
     return (
         <SessionProvider session={session}>
-            <CacheProvider value={emotionCache}>
+            <GlobalStateProvider>
 
-                <Head>
-                    <meta name="viewport" content="initial-scale=1, width=device-width"/>
-                </Head>
-                <ThemeProvider theme={theme}>
-                    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-                    <CssBaseline/>
-                    <Component {...pageProps} />
-                </ThemeProvider>
-            </CacheProvider>
+                <CacheProvider value={emotionCache}>
+                    <Head>
+                        <meta name="viewport" content="initial-scale=1, width=device-width"/>
+                    </Head>
+                    <ThemeProvider theme={theme}>
+                        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+                        <CssBaseline/>
+                        <Component {...pageProps} />
+                    </ThemeProvider>
+                </CacheProvider>
+            </GlobalStateProvider>
         </SessionProvider>
+
     );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,17 +2,16 @@ import * as React from 'react';
 import type {NextPage} from 'next';
 import Typography from '@mui/material/Typography';
 import {signIn, useSession} from "next-auth/react";
-import StartScreen from "../src/components/StartScreen";
+import StartScreen from "../components/StartScreen";
 import {useState} from "react";
-import SelectPlaylist from "../src/components/SelectPlaylist";
-import LoadingResults from "../src/components/LoadingResults";
-import Results from "../src/components/Results";
-import Error from "../src/components/Error";
+import SelectPlaylist from "../components/SelectPlaylist";
+import LoadingResults from "../components/LoadingResults";
+import Results from "../components/Results";
+import Error from "../components/Error";
 import {Container} from "@mui/material";
 
 type AppStage = 'start' | 'selectPlaylist' | 'loadingResults' | 'results' | 'error';
 const Home: NextPage = () => {
-    const {data: session} = useSession();
     const [stage, setStage] = useState<AppStage>('start');
     const [playlistId, setPlaylistId] = useState<string | null>(null);
     const getComponentFromStage = () => {
@@ -25,9 +24,9 @@ const Home: NextPage = () => {
                     setPlaylistId(playlistId);
                 }}/>;
             case 'loadingResults':
-                return <LoadingResults playlistId={playlistId} onFinish={()=>setStage('results')}/>;
+                return <LoadingResults playlistId={playlistId} onFinish={() => setStage('results')}/>;
             case 'results':
-                return <Results playlistId={playlistId} onFinish={()=>{
+                return <Results playlistId={playlistId} onFinish={() => {
                     setStage('selectPlaylist');
                     setPlaylistId(null);
                 }}/>;
